811a7bef48e125019ae03b080612dd268402dedc
hejiajun <1765393959@qq.com>
2024-05-29 23:37:32 +0800

    first commit

---
+++ fairyoj-backend-common/src/main/java/com/hjj/fairyojbackendcommon/constant/RabbitMQConstant.java
@@ -0,0 +1,7 @@
+package com.hjj.fairyojbackendcommon.constant;
+
+public interface RabbitMQConstant {
+    String COMMON_QUEUE = "oj_common_queue"; // 普通队列名称
+    String COMMON_EXCHANGE = "oj_common_exchange"; // 普通交换机名称
+    String COMMON_ROUTING_KEY = "oj_common_routingKey"; // 普通routingKey
+}
